f5d64910bd0d365bcbcd9fe409390db7f7bc4b6b
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2020-01-20 22:10:46 -0800

    changed data_out to reg

--- romulator/diagnostics.v
+++ romulator/diagnostics.v
@@ -1,3 +1,19 @@
+// ROMulator - RAM/ROM replacement and diagnostic for 8-bit CPUs
+// Copyright (C) 2019  Michael Hill
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 module diagnostics(
      output reg halt,   // io line to halt the cpu
 
@@ -12,7 +28,7 @@ module diagnostics(
      // ram control
      output reg [15:0] address,
      input      [7:0] data,
-     output     [7:0] data_out,
+     output reg [7:0] data_out,
      output reg we,
      output reg cs,
 
