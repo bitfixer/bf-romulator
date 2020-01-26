241b815399baad44fa7d94507bc5411dd12beb7e
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2020-01-26 12:10:28 -0800

    modified setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-apt-get update
-apt-get install python3-pip
-apt-get install git
-apt-get install wiringpi
-apt-get install wget
-pip3 install -U apio
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apt-get install git
+sudo apt-get install wiringpi
+sudo apt-get install wget
+sudo pip3 install -U apio
 apio install system icestorm scons iverilog
+git clone https://github.com/bitfixer/bf-romulator.git
