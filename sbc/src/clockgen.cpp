aeb46b59225adb3665bf2d608972ba7e3c01d1d7
Michael Hill <bitfixer@bitfixer.com>
2021-07-27 17:00:34 -0700

    added reset signal

--- sbc/src/clockgen.cpp
+++ sbc/src/clockgen.cpp
@@ -1,7 +1,16 @@
 #include <avr/io.h>
+#include <avr/delay.h>
 
 int main(void)
 {
+    // set PB3 as output
+    DDRB = 1 << PB3;
+
+    // SET reset low
+    PORTB = 0;
+    _delay_ms(1200);
+    PORTB = 1 << PB3;
+
     while(1)
     {
         
