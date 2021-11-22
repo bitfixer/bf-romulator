0f79edb4617c1c90b21a1a389a7512ad767710e1
Michael Hill <mhill@twitter.com>
2021-11-22 12:35:22 -0800

    cosmetic changes to web virtual display

--- webserver/draw.js
+++ webserver/draw.js
@@ -3,11 +3,7 @@ window.onload = function() {
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-var width = canvas.width;
-var height = canvas.height;
-
-var imagedata = ctx.createImageData(width, height);
-
+var imagedata = ctx.createImageData(canvas.width, canvas.height);
 var bitmapArray = new Uint8Array(64000);
 var characterRom;
 var scale = 2;
@@ -64,7 +60,7 @@ function createImage()
                         {
                             for (xx = xdest; xx < xdest + scale; xx++)
                             {
-                                var pixelIndex = ((yy * width) + xx) * 4;
+                                var pixelIndex = ((yy * canvas.width) + xx) * 4;
                                 imagedata.data[pixelIndex] = val;
                                 imagedata.data[pixelIndex+1] = val;
                                 imagedata.data[pixelIndex+2] = val;
@@ -93,6 +89,49 @@ function createImage()
     xhttp.send(null);
 }
 
+function resize()
+{
+    var width = document.documentElement.clientWidth;
+    var height = document.documentElement.clientHeight;
+
+    // check scaling for the canvas
+    var newWidth;
+    var newHeight;
+    if (width >= 1280 && height >= 800)
+    {
+        newWidth = 1280;
+        newHeight = 800;
+        scale = 4;
+    }
+    else if (width >= 640 && height >= 400)
+    {
+        newWidth = 640;
+        newHeight = 400;
+        scale = 2;
+    }
+    else
+    {
+        newWidth = 320;
+        newHeight = 200;
+        scale = 1;
+    }
+
+    if (newWidth != canvas.width || newHeight != canvas.height)
+    {
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+        imagedata = ctx.createImageData(canvas.width, canvas.height);
+    }
+
+    var xMargin = width > canvas.width ? (width - canvas.width) / 2 : 0;
+    var yMargin = height > canvas.height ? (height - canvas.height) / 2 : 0;
+
+    canvas.style.left = xMargin + "px";
+    canvas.style.top = yMargin + "px";
+}
+
+window.addEventListener('resize', resize);
 loadRom();
+resize();
 
 }
