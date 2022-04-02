013b0b6157d158897d44807685625d617384320e
Michael Hill <bitfixer@bitfixer.com>
2022-04-02 16:51:19 -0700

    added linux setup script

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,8 +1,8 @@
-apt-get -y update
-apt-get -y python3-pip
-apt-get -y install git
-apt-get -y install wget
-pip3 install -u apio==0.5.4
+sudo apt-get -y update
+sudo apt-get -y install python3-pip
+sudo apt-get -y install git
+sudo apt-get -y install wget
+sudo pip3 install apio==0.5.4
 apio install -a
 git clone https://github.com/bitfixer/bf-romulator.git
 cd bf-romulator; git submodule init; git submodule update; cd ..
