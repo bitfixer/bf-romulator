a72e26bad1e55b727094aff7b45b75b8851d8b8e
Michael Hill <mhill@twitter.com>
2022-04-03 16:03:25 -0700

    setup script for mac

---
+++ setup_mac.sh
@@ -0,0 +1,8 @@
+brew install python
+brew install git
+brew install wget
+sudo pip3 install apio==0.5.4
+apio install -a
+git clone https://github.com/bitfixer/bf-romulator.git
+cd bf-romulator; git submodule init; git submodule update; cd ..
+cd bf-romulator/cc65; make
