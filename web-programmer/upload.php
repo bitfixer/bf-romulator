da09e3ac59b04a51f698433fb9e26f8146303a18
Michael Hill <bitfixer@bitfixer.com>
2022-05-13 15:31:06 -0700

    enable table working

--- web-programmer/upload.php
+++ web-programmer/upload.php
@@ -4,19 +4,9 @@
 <?php
 
 function parse_memory_set($fname, $dirname, $outfname) {
-
     $nummaps = 32;
     $mapsize = $nummaps * 65536;
-    //$memorymaps = array();
-    //for ($i = 0; $i < $mapsize; $i++)
-    //{
-    //    $memorymaps[$i] = 0;
-    //}
-
     $memorymaps = str_repeat(pack("C", "0"), $mapsize);
-
-    //printf("s: %d<br>\n", sizeof($memorymaps));
-    
     // parse each line
     foreach (file($fname) as $line) {
         if (strlen($line) < 2) {
@@ -36,43 +26,162 @@ function parse_memory_set($fname, $dirname, $outfname) {
         $index = $parts[0];
         // get address
         $address = base_convert($parts[2], 16, 10);
-        printf("index: %d, address %s<br>\n", $index, $address);
-
         $rom_fname = sprintf("%s/%s", $dirname, $parts[1]);
         if (file_exists($rom_fname))
         {
             $rom_contents = file_get_contents($rom_fname);
-            printf("size of %s: %d<br>\n", $rom_fname, strlen($rom_contents));
-
             // copy contents into memory map
             $start_addr = ($index * 65536) + $address;
-            printf("copying to %d<br>\n", $start_addr);
-
             // now copy the data
             for ($b = 0; $b < strlen($rom_contents); $b++)
             {
-                //printf("writing to %d, content %d %s %X<br>\n", $b, $rom_contents[$b]);
-                //$a = unpack("Ccontents", $rom_contents[$b]);
-                //$memorymaps[$start_addr + $b] = $a["contents"];
-                //printf("got %d %d\n", $b, $memorymaps[$start_addr + $b]);
                 $memorymaps[$start_addr + $b] = $rom_contents[$b];
             }
         }
     }
 
-    //printf("s: %d<br>\n", sizeof($memorymaps));
-    // now write entire memory map contents to file
-    /*
-    $fp = fopen($outfname, "wb");
-    for ($b = 0; $b < $mapsize; $b++)
-    {
-        printf("%d %X<br>\n", $b, $memorymaps[$b]);
-        fwrite($fp, pack("C*", $memorymaps[$b]));
-    }
-    fclose($fp);
-    */
-
     file_put_contents($outfname, $memorymaps);
+}
+
+function get_region_type($region_str) {
+    $READWRITE =    "0b01010";
+    $WRITETHROUGH = "0b00111";
+    $READONLY =     "0b01001";
+    $PASSTHROUGH =  "0b00101";
+    $VRAM =         "0b10111";
+    $INACTIVE =     "0b00000";
+
+    printf("get region: %s<br>", $region_str);
+
+    if (strpos($region_str, "readwrite")) {
+        return $READWRITE;
+    } else if (strpos($region_str, "writethrough")) {
+        return $WRITETHROUGH;
+    } else if (strpos($region_str, "readonly")) {
+        return $READONLY;
+    } else if (strpos($region_str, "passthrough")) {
+        return $PASSTHROUGH;
+    } else if (strpos($region_str, "vram")) {
+        return $VRAM;
+    }
+
+    return $INACTIVE;
+}
+
+function parse_enable_table($fname, $outfname) {
+    $VRAM = "0b10111";
+    $ADDR_GRANULARITY_SIZE = 256;
+    $MAX_VRAM_SIZE = 2048;
+    $NUMMAPS = 32;
+
+    $granularity = $ADDR_GRANULARITY_SIZE;
+
+    $num_address_entries = pow(2, 16) / $granularity;
+    $addr_entry_bits = log($num_address_entries, 2);
+
+    // get number of bits needed to represent the number of maps
+    $config_bits = log($NUMMAPS, 2);
+
+    // total bits needed for every entry in address enable table
+    $num_entry_bits = $addr_entry_bits + $config_bits + 1;
+    $num_entries = pow(2, $num_entry_bits);
+
+    $table = str_repeat(pack("C", 0), $num_entries);
+
+    $vram_start_addr = array();
+    $vram_end_addr = array();
+
+    for ($i = 0; $i < $NUMMAPS; $i++)
+    {
+        $vram_start_addr[$i] = 0;
+        $vram_end_addr[$i] = 0;
+    }
+
+    foreach (file($fname) as $line) {
+        if (strlen($line) < 2) {
+            continue;
+        }
+
+        // skip comments
+        if ($line[0] == '#' || ($line[0] == '/' && $line[1]) == '/') {
+            continue;
+        }
+
+        $parts = explode(",", $line);
+        print_r($parts);
+
+        $start_map_index = -1;
+        $end_map_index = -1;
+        if (strpos($parts[0], "-")) {
+            sscanf($parts[0], "%d-%d", $start_map_index, $end_map_index);
+        } else {
+            $start_map_index = $parts[0];
+            $end_map_index = $start_map_index;
+        }
+
+        print_r($line);
+        printf("\n<br>%d %d<br>\n", $start_map_index, $end_map_index);
+
+        // get address
+        $addr = base_convert($parts[1], 16, 10);
+        $end_addr = base_convert($parts[2], 16, 10);
+        $region_type = get_region_type($parts[3]);
+
+        printf("%d %d %s<br>\n", $addr, $end_addr, $region_type);
+
+        if ($region_type == $VRAM)
+        {
+            //printf("VRAM region<br>\n");
+            // TBD, not implemented yet
+        }
+        else
+        {
+            for ($map_index = $start_map_index; $map_index <= $end_map_index; $map_index++)
+            {
+                for ($address = $addr; $address <= $end_addr; $address += $granularity)
+                {
+                    for ($rw = 0; $rw < 2; $rw++)
+                    {
+                        // get table address
+                        // address has the following bit pattern:
+                        // config(config_bits), rw, addr(addr_entry_bits)
+                        $config_index = $map_index;
+                        $table_addr = $config_index;
+                        $table_addr <<= 1;
+                        $table_addr += $rw;
+                        $table_addr <<= $addr_entry_bits;
+
+                        // get high bits of address to get the index of the address entry
+                        $addr_shift = 16 - $addr_entry_bits;
+                        $entry_addr = $address >> $addr_shift;
+                        $table_addr += $entry_addr;
+
+                        // get bit pattern for this entry
+                        // 2 higher bits are the read (high) value
+                        // 2 lower bits are the write value
+                        $byteval = 0;
+
+                        printf("table addr %d<br>\n", $table_addr);
+                        printf("region type %s<br>\n", $region_type);
+                        if ($rw == 1) {
+                            $region_val = (int)base_convert($region_type, 2, 10);
+                            $mask_val = (int)base_convert("0b01100", 2, 10);
+                            $byteval = ($region_val & $mask_val) >> 2;
+                        } else {
+                            $region_val = (int)base_convert($region_type, 2, 10);
+                            $mask_val = (int)base_convert("0b00011", 2, 10);
+                            $byteval = $region_val & $mask_val;
+                        }
+                        //fprintf(stderr, "address %X, rw %d, ci %d, as %d, table_addr %d %X, r %d %X bv %X\n", address, rw, config_index, addr_shift, table_addr, table_addr, region_type, region_type, byteval);
+                        $table[$table_addr] = pack("C", $byteval);
+                    }
+                }
+            }
+        }
+    }
+
+    // write table to file
+    file_put_contents($outfname, $table);
 }
 
 
@@ -147,7 +256,9 @@ if (true) {
 
     // start with memory set
     parse_memory_set($memory_set, $dirname, "test/memoryset.bin");
-    // done
+    
+    // create enable table
+    parse_enable_table($enable_table, "test/enable_table.bin");
 
 } else { 
     echo '
